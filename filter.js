b5271db03e6e0a2370a26abf22cd4c08d0ca9445
Adrian Walker <awalker3365@gmail.com>
2021-04-17 16:31:13 -0400

    next

---
+++ filter.js
@@ -0,0 +1,17 @@
+// 1.)
+
+const numbers = [1,8,9,15,2,3,27]
+const overFive = numbers.filter(num => num > 5)
+console.log(overFive)
+
+// 2.)
+
+const numberz = [1,5,2,4,87,24,66]
+const evenNumbers = numberz.filter(even => even % 2 === 0)
+console.log(evenNumbers)
+
+// 3.)
+
+const things = ["Cat", "Puppies", "Shoe", "Hippos", "Cars", "Hat"]
+const underFive = things.filter(string => string = things.length > 5)
+console.log(underFive)
